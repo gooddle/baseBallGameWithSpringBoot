b9fdc4ed321a3df139bd7e7710fa7bb18373790b
gooddle <vptl6559@gmail.com>
2024-10-26 00:48:34 +0900

    :zap: add user passwordEncoder

---
+++ src/main/java/com/java/game/infra/security/PasswordEncoderConfig.kt
@@ -0,0 +1,15 @@
+package com.java.game.infra.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+open class PasswordEncoderConfig {
+
+    @Bean
+    open fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+}
